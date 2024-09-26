553ad9bdb0da530f24e0589df5af3a8731ac1553
NavaneethaMella <mellanavaneetha07@gmail.com>
2024-09-26 15:24:34 +0530

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import './App.css';
-import Usecallback from './Usecallback'
-import { useCallback, useState } from 'react';
+import Usereducer from './Usereducer'
 function App() {
-  const [count , setCount] =useState(0)
-  const newFn= useCallback(()=>{},[])
   return (
     <div className="App">
-     <Usecallback newFn={newFn}/>
-     <h1>{count}</h1>
-     <button onClick={()=>setCount(prev => prev + 1)}>click on</button>
+   <Usereducer></Usereducer>
      
     </div>
   );
