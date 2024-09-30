eaaad5ce2fde190e5bfe0d5353a3bed0c9701207
NavaneethaMella <mellanavaneetha07@gmail.com>
2024-09-30 11:44:07 +0530

    Formred

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './App.css';
 import Forms from './Forms'
 import  Formyup from './Formyup'
+import Formredboil from './Formredboil'
 function App() {
   return (
     <div className="App">
    <Forms></Forms>
+
    <Formyup></Formyup>
+
+   <Formredboil></Formredboil>
    
     </div>
   );
